792644412c45d7598e1f5f8f968e84f9a7028bb9
Solomon Fosuhene <s2huncho@gmail.com>
2025-02-23 15:01:58 +0000

    Added taskbar to show app icons in workspace

--- waybar/scripts/power.sh
+++ waybar/scripts/power.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-# Rofi Menu
-CHOICE=$(echo -e " Lock\n Sleep\n Restart\n Shutdown\n Log Out" | rofi -dmenu -p "Power Options")
+#!/bin/bash
 
-case "$CHOICE" in
-    " Lock")
-        # Lock the screen
-        hyprlock ;;
-    " Sleep")
-        # Suspend system
-        systemctl suspend ;;
-    " Restart")
-        # Restart system
-        systemctl reboot ;;
-    " Shutdown")
-        # Power off system
-        systemctl poweroff ;;
-    " Log Out")
-        # Log out from Hyprland
-        hyprctl dispatch exit ;;
-    *)
-        # If cancelled or no valid option selected
-        exit 1 ;;
+# Define options for the Rofi menu
+options=" Lock\n Logout\n Reboot\n Shutdown\n Suspend"
+
+# Show Rofi menu
+chosen=$(echo -e "$options" | rofi -dmenu -p "Power Menu" -theme-str 'window { width: 250px; }')
+
+# Execute selected action
+case "$chosen" in
+    " Lock") hyprlock ;;
+    " Logout") hyprctl dispatch exit ;;
+    " Reboot") systemctl reboot ;;
+    " Shutdown") systemctl poweroff ;;
+    " Suspend") systemctl suspend ;;
+    *) exit 1 ;;
 esac
+
+# esac
 
