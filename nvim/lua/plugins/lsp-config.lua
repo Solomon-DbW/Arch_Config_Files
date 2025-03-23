399512811092b60d501a654bd4ec7b9752c8dceb
Solomon Fosuhene <s2huncho@gmail.com>
2025-03-23 15:47:45 +0000

    Updates commit

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,53 +1,52 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "clangd", "lua_ls", "marksman", "pyright", "rust_analyzer", "html", "cssls", "ts_ls" },
-                -- ensure_installed = { "clangd", "lua_ls", "marksman", "pyright", "rust_analyzer" },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.marksman.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.rust_analyzer.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.html.setup({
-                capabilities = capabilities,
-                cmd = { "vscode-html-language-server", "--stdio" }
-            })
-            lspconfig.cssls.setup({
-                capabilities = capabilities,
-                cmd = { "vscode-css-language-server", "--stdio" }
-            })
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "clangd", "lua_ls", "marksman", "pyright", "rust_analyzer", "html", "cssls", "ts_ls", "emmet_ls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.marksman.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+        cmd = { "vscode-html-language-server", "--stdio" },
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+        cmd = { "vscode-css-language-server", "--stdio" }
+      })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
