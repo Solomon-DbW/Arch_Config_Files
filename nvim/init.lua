678396ce2ea9fe6810ff3f4b0efbed0c28f786aa
Solomon Fosuhene <s2huncho@gmail.com>
2025-04-08 16:47:33 +0100

    Added scrollbar and web browser-like tabs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,3 @@
 require('config.lazy')
 require("vim-options")
 
--- FOR LOADING HTML IN BRAVE --
-vim.api.nvim_create_user_command('OpenInBrave', function()
-    local file = vim.fn.expand('%:p')
-    if file:match('%.html$') then
-        vim.fn.jobstart({'brave', file}, {detach = true})
-    else
-        print("Not an HTML file")
-    end
-end, {})
-
--- Keybinding: Press <leader>o to open in Brave
-vim.api.nvim_set_keymap('n', '<leader>o', ':OpenInBrave<CR>', { noremap = true, silent = true })
-
--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -28,10 +28,22 @@ return {
   },
   config = function()
     -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-    vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-    vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-    vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-    vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+    -- vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+    -- vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+    -- vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+    -- vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "",
+          [vim.diagnostic.severity.WARN] = "",
+          [vim.diagnostic.severity.INFO] = " ",
+          [vim.diagnostic.severity.HINT] = "󰌵",
+        },
+        -- Preserve your highlight groups
+        severity_sort = true,
+      }
+    })
     vim.keymap.set("n", "<leader>nt", ":Neotree<CR>")
     vim.keymap.set("n", "<leader>nc", ":Neotree close<CR>")
 
